1398cbc449b3620c14e48f28e55b2c8143329966
JoycelineMarealle <joyceline.marealle@peopleshores.com>
2024-09-10 14:45:54 -0400

    Ex 3 oddlist

---
+++ PYTHON/DataStructure/OddNumbers.py
@@ -0,0 +1,13 @@
+#Ask user for input
+user = int (input("Enter a number :"))
+
+#Create a list of all odd numbers between 1 and a max number.
+# Max number is something you need to take from a user using input() function
+
+odd_list = []
+for i in range( 1, user):
+    if i%2 !=0:
+        odd_list.append(i)
+print(odd_list)
+
+
