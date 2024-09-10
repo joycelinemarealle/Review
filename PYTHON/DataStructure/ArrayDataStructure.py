8a95eaea1d95829799ec6c864a5c2b59d4bcc550
JoycelineMarealle <joyceline.marealle@peopleshores.com>
2024-09-10 13:00:58 -0400

    python files upload

---
+++ PYTHON/DataStructure/ArrayDataStructure.py
@@ -0,0 +1,57 @@
+#Create a dictionary holding expenses
+expense = {
+                   "Jan": 2200,
+                   "Feb" : 2350,
+                   "Mar" : 2600,
+                  " Apr" : 2130,
+                   "May": 2190}
+print()
+expense1 = {
+                   1: 2200,
+                   2 : 2350,
+                   3 : 2600,
+                   4 : 2130,
+                   5: 2190}
+print ("Monthly expenses",expense1)
+print()
+
+# 1 In Feb, how many dollars you spent extra compare to January?
+amount = expense["Feb"] - expense["Jan"]
+print( "Spent $ in February ",amount, "than January")
+
+# 2. Find out your total expense in first quarter (first three months) of the year.
+    #dictionaries are not sequences so store in numbers so it is easier to sort
+    #Get list of keys and sort them
+Ks = list(expense1.keys())
+Ks.sort()
+print()
+
+    #loop through first 3 keys and sum
+sum = 0
+count = 0
+for i in Ks:
+    count += 1
+    sum += expense1[i]
+    if count == 3 :
+        break
+print("The first quarter expense :$", sum)
+print()
+
+# 3. Find out if you spent exactly 2000 dollars in any month
+for i in Ks:
+    if expense1[i] == 2000:
+        print ("There is a month where I spent exactly $", 2000)
+    #
+print (2000 in expense1,", No month expense of $2000")
+print()
+
+# 4. June month just finished and your expense is 1980 dollar. Add this item to our monthly expense list
+expense1[6] = 1980
+print ("June month expense added",expense1)
+print()
+
+# 5. You returned an item that you bought in a month of April and
+   # got a refund of 200$. Make a correction to your monthly expense list based on this
+expense1[4] = expense1[4] -200
+print("New expenses after April refund",expense1)
+print()
---
+++ PYTHON/DataStructure/Dictionary.py
@@ -0,0 +1,11 @@
+D= {}
+D["name"] = "joy"
+D["job"] = "software engineer"
+D["age"] = 27
+print(D)
+
+employee1 = dict (name = "Eve", job = "dev", age =27)
+print (employee1)
+
+employee2 = dict (zip(["name", "job", "age"], ["Elton", "dev", 28]))
+print (employee2)
