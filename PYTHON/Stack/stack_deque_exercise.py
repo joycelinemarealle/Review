0ef2463f941e13b38871f2c5bb107c6f20d2a2be
JoycelineMarealle <joyceline.marealle@peopleshores.com>
2024-09-17 13:36:27 -0400

    Stack deque and list Ex 1 and 2

--- PYTHON/Stack/stack_deque_exercise.py
+++ PYTHON/Stack/stack_deque_exercise.py
@@ -3,7 +3,6 @@
 
 from collections import deque
 
-sentence = "We will conquer COVID-19"
 
 class Stack:
     def __init__(self):
@@ -13,7 +12,7 @@ class Stack:
         self.container.append(val)
 
     def pop (self):
-        self.container.pop()
+        return self.container.pop()
 
     def peek(self):
         return self.container[-1]
@@ -24,3 +23,60 @@ class Stack:
     def size (self):
         return len(self.container)
 
+
+def reverse_string(s):
+    #s a string
+        stack = Stack ()
+
+        for char in s:
+            stack.push(char)
+
+        rstr = ''
+        while stack.size()!=0: #stack not empty
+            rstr += stack.pop()
+        return rstr
+
+#push the word
+#then have empty dequer
+#split sentence into letters
+#letter popped added to this new deque
+#print deque
+
+
+#2 Write a function in python that checks if parenthesis in the string are balanced or not.
+# Possible parantheses are "{}',"()" or "[]".
+
+#go through each element in a sentence
+#check for all types of parenthiss
+#for each type check if even then return true
+#if not even return false
+
+
+
+
+def is_balanced(s):
+    stack = Stack()
+    match_dict = {
+        '{': '}',
+        '[': ']',
+        '(': ')'}
+   #loop through string
+    for char in s:
+        if char in '{([': #if character opening bracket push it to stack
+            stack.push(char)
+        elif char in ' })]': #if it is closing brackets
+            if stack.size() ==0 or  match_dict[stack.pop()] != char:
+                return False #unbalanced brackets
+    #stack is now empty and all parenthesis balanced
+    return stack.size() ==0
+
+
+
+if __name__ == '__main__':
+    print(reverse_string('We will conquere COVID-19'))
+    print(is_balanced("({a+b})"))
+    print(is_balanced("))((a+b}{"))
+    print(is_balanced("((a+b))"))
+    print(is_balanced("((a+g))"))
+    print(is_balanced("))"))
+    print(is_balanced("[a+b]*(x+2y)*{gg+kk}"))
