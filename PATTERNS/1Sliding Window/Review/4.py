600496d6790fe64726bcc176fc4eab83dacb8b58
JoycelineMarealle <joyceline.marealle@peopleshores.com>
2024-10-07 10:26:03 -0400

    sliding window: review 5

--- PATTERNS/1Sliding Window/Review/4.py
+++ PATTERNS/1Sliding Window/Review/4.py
@@ -3,8 +3,6 @@ def longest_substring_with_distinct_characters(str,k):
     window_start = 0
     max_length = 0
     char_freq = {}
-
-
     #2 Slide window
     for window_end in range (len(str)):
         right_char = str[window_end]
@@ -15,20 +13,21 @@ def longest_substring_with_distinct_characters(str,k):
         else:
              char_freq[right_char] += 1
 
-        #4 max_length = max(max_length,window_end -window_start+1)
-        max_length = max(max_length, window_end-window_start+1)
-
-        #5 Shrink window size when > the K distinct Char
+        #4 Shrink window size when > the K distinct Char
         while len(char_freq) > k:
             left_str = str[window_start]
-            # 6 re-balance hashmap
+            #5 re-balance hashmap
             char_freq[left_str] -=1
 
             if char_freq[left_str] == 0: #if zero then remove
                 del char_freq[left_str]
 
-            #7 move ahead
+            #6 move ahead
             window_start +=1
 
+            # 8 max_length = max(max_length,window_end -window_start+1)
+        max_length = max(max_length, window_end - window_start + 1)
     return max_length
 
+if __name__ == '__main__':
+    print (longest_substring_with_distinct_characters("araaci",2))
---
+++ PATTERNS/1Sliding Window/Review/5.py
@@ -0,0 +1,32 @@
+def max_fruits_in_basket(fruits):
+    #1 Initialization
+    window_start = 0
+    max_length = 0
+    fruit_freq = {}
+
+    #2 Slide window in range of fruits
+    for window_end in range (len(fruits)):
+        #3 Process each fruit to fill in hashmap
+        right_fruit = fruits[window_end]
+
+        if right_fruit not in fruit_freq:
+            fruit_freq[right_fruit] = 1
+        else:
+            fruit_freq[right_fruit] +=1
+
+        #Shrink whenever len of hashmap > 2
+        while len(fruit_freq) >2:
+            #Re-balance hashmap when left element goes out
+            left_char = fruits[window_start]
+            fruit_freq[left_char] -=1
+            if fruit_freq[left_char] == 0:
+                del fruit_freq[left_char]
+           #move window ahead
+            window_start+=1
+
+        #Track max lenght
+        max_length = max (max_length, window_end-window_start+1)
+    return max_length
+if __name__ == '__main__':
+    fruits = ['A', 'B', 'C', 'A', 'C']
+    print(max_fruits_in_basket(fruits))
