ceec321f550331083b620ae8446e220d5052d21f
JoycelineMarealle <joyceline.marealle@peopleshores.com>
2024-10-22 11:39:32 -0400

    2 Pointer : 5 Triplet Sum Close to Target

---
+++ PATTERNS/2 Two Pointers/Problems/Triplet to Sum Close to Target.py
@@ -0,0 +1,30 @@
+import math
+def triplet_close_to_target(arr, target_sum):
+    #sort the array
+    arr.sort()
+    smallest_difference = math.inf
+
+    for i in range (len(arr)-2):
+        left = i+1 #start at second element
+        right = len(arr) -1 #right pointer index
+
+        while (left < right):
+            target_diff = target_sum - arr[i] - arr[left] - arr[right]
+
+            if target_diff == 0: #we found a triplet with exact sum
+                return target_sum - target_diff
+
+            #the second part of following if is to handle the smallest sum when we have more than one solution
+            if abs(target_diff) < abs(smallest_difference) or abs(target_diff) == abs(smallest_difference):
+                smallest_difference = target_diff
+
+            if target_diff > 0:
+                left +=1
+
+            else:
+                right -=1
+    return target_sum-smallest_difference
+
+if __name__ == '__main__':
+    print(triplet_close_to_target([-2,0,1,2], 2))
+    print(triplet_close_to_target([-3, -1, 1,2], 1))
