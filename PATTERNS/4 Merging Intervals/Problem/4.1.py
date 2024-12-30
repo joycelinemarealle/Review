e082bac4afb8d0344a923aa69b508d1abe78e332
joy <joyceline.marealle@peopleshores.com>
2024-12-29 19:51:00 -0500

    Pattern 4 Merging Intervals: 4.1 Return Conflicting appointments

--- PATTERNS/4 Merging Intervals/Problem/4.1.py
+++ PATTERNS/4 Merging Intervals/Problem/4.1.py
@@ -4,16 +4,17 @@ def conflicting_appointments(intervals):
     start, end = 0,1
 
     # 2sort the intervals
-    intervals.sort(key=lambda x: x[0])
+    intervals.sort(key=lambda x: x[start])
 
     #3Travers through list of intervals to find overlapping b.start < a.end (previous interval
     for i in range(1,len(intervals)):
         #if two intervals overlap
         if intervals[i][start] < intervals[i-1][end]:
-            conflicting_appointments.append(intervals[i])
-
-        #if two intervals overlap store in new list
-
+            # add both previous and current interval if not in new list
+            if intervals[i-1] not in conflicting_appointments :
+                conflicting_appointments.append(intervals[i-1])
+                if intervals[i] not in conflicting_appointments:
+                    conflicting_appointments.append(intervals[i])
     #return new list
     return conflicting_appointments
 
